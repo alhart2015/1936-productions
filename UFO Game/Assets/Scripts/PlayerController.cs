119fdd8d967cb98afdfe07ed7adc55e91ab9dca2
Alden Hart <alden.hart9@gmail.com>
2017-08-08 19:55:12 -0400

    Capitalize axis names smh

--- UFO Game/Assets/Scripts/PlayerController.cs
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -17,8 +17,8 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis("horizontal");
-		float moveVertical = Input.GetAxis("vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rigidBody2d.AddForce (movement);
 	}
