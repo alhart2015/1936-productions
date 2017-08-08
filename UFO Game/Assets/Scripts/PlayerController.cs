d11771fdf137ecf3c98badac5e26d6ff0f83b845
John Clougherty <john.clougherty@gmail.com>
2017-08-08 19:57:53 -0400

    Adding Player motion to the player

--- UFO Game/Assets/Scripts/PlayerController.cs
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed;
+
 	private Rigidbody2D rb2d;
 
 	void Start()
@@ -16,6 +18,6 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement);
+		rb2d.AddForce (movement * speed);
 	}
 }
